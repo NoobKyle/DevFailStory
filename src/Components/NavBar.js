843342b196822bd8211f1eb388afe16f82d4cc8f
kyle <kadangotinaishe5@gmail.com>
2021-03-24 20:15:52 +0200

    Small Changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,82 +2,26 @@
 import React from "react";
 import {
   Navbar,
-  NavbarToggler,
   NavbarBrand,
   Nav,
   NavItem,
   NavLink,
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText,
-  FormInput,
-  Collapse
 } from "shards-react";
 
 export default class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggleDropdown = this.toggleDropdown.bind(this);
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-
-    this.state = {
-      dropdownOpen: false,
-      collapseOpen: false
-    };
-  }
-
-  toggleDropdown() {
-    this.setState({
-      ...this.state,
-      ...{
-        dropdownOpen: !this.state.dropdownOpen
-      }
-    });
-  }
-
-  toggleNavbar() {
-    this.setState({
-      ...this.state,
-      ...{
-        collapseOpen: !this.state.collapseOpen
-      }
-    });
-  }
-
   render() {
     return (
-      <Navbar type="dark" theme="primary" expand="md">
-        <NavbarBrand href="#">Developer-Fail-Story</NavbarBrand>
-        <NavbarToggler onClick={this.toggleNavbar} />
-
-        <Collapse open={this.state.collapseOpen} navbar>
+        <Navbar type="light" expand="md" className='navigationbar'>
+          <NavbarBrand href="/" className='ml-auto'><strong>Developer-Fail-Story</strong></NavbarBrand>
           <Nav navbar className='ml-auto'>
-            <NavItem>
-              <NavLink active href="#">
-                Active
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#" disabled>
-                Disabled
-              </NavLink>
-            </NavItem>
+              <NavItem>
+                <NavLink active>
+                  BETA v1.0
+                </NavLink>
+              </NavItem>
           </Nav>
-
-          <Nav navbar className="ml-auto">
-          <NavItem>
-            <NavLink active>
-              BETA v1
-            </NavLink>
-          </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+          <div className='ml-auto'></div>
+        </Navbar>
     );
   }
 }
