2f11935b33c9c7d65d608258f734632e990d892a
kyle <kadangotinaishe5@gmail.com>
2021-03-27 10:35:57 +0200

    Small Changes

--- src/Pages/Signup/signup.js
+++ src/Pages/Signup/signup.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  Navbar,
   NavbarBrand,
   Nav,
   NavItem,
@@ -12,22 +11,13 @@ import {
 } from "shards-react";
 import './signup.css';
 
-export default class Signup extends React.Component {
+import Navbar from '../../Components/NavBar';
 
+export default class Signup extends React.Component {
   render() {
     return (
     <div>
-      <Navbar type="light" expand="md" className='navigationbar'>
-        <NavbarBrand href="/" className='ml-auto'><strong>Developer-Fail-Story</strong></NavbarBrand>
-        <Nav navbar className='ml-auto'>
-            <NavItem>
-              <NavLink active>
-                BETA v1.0
-              </NavLink>
-            </NavItem>
-        </Nav>
-        <div className='ml-auto'></div>
-      </Navbar>
+      <Navbar />
 
       <h3 className='headertext'>Share and learn from the developer failure stories</h3>
       <p className='headersubtext'>A community for lifelong learners to share and discover their journeys towards learning something new.</p>
