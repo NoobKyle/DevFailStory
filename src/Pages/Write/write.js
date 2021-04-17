985c374840ed68cb5be2a3a36e06bc6e03d0748e
kyle <kadangotinaishe5@gmail.com>
2021-04-18 00:24:03 +0200

    Title & Description + Local State

--- src/Pages/Write/write.js
+++ src/Pages/Write/write.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import Editor from "rich-markdown-editor";
 import {
+  Button,
   Card,
   CardBody,
   Container,
   Row,
   Col,
+  FormInput,
+  FormTextarea
 } from "shards-react";
 import './write.css';
 
@@ -13,10 +16,19 @@ import Navbar from '../../Components/NavBar';
 
 const Write: React.FC = () => {
 
-    const [editor, setEditor] = useState('');
+    const [editor, setEditor] = useState('No Content');
+    const [title, setTitle] = useState('No Title');
+    const [description, setDescription] = useState('No Description');
 
     function logeditorstate(){
+      console.log(title);
+      console.log(description);
       console.log(editor);
+    }
+
+    function SaveContent(){
+      console.log('Process : Article Saved');
+      
     }
 
     return (
@@ -25,52 +37,54 @@ const Write: React.FC = () => {
 
       <Container>
         <Row>
-            <Col sm="12" md="4" lg="8">
-              <Card className='writearea'>
-                  <CardBody>
-                    <Editor
-                      defaultValue="Hello world!"
-                      className='editorarea'
-                      onChange={ (e) => {setEditor(e()); logeditorstate()} }
-                      />
+          <Col sm="12" md="4" lg="8">
+            <Card className='writearea'>
+              <CardBody>
+                  <Editor
+                    autoFocus={true}
+                    defaultValue="Hello world!"
+                    className='editorarea'
+                    onChange={ (e) => {setEditor(e()); logeditorstate()} }
+                    />
+              </CardBody>
+            </Card>
+          </Col>
 
-                  </CardBody>
-              </Card>
-            </Col>
-
-            <Col>
-                <Row>
-              <Card className='feedarea'>
-                  <CardBody>
-                    <h5>Instructions</h5>
+          <Col>
+              <Row>
+                <Card className='feedarea'>
+                  <CardBody className='titlearea'>
+                    <h5>Title</h5>
+                    <FormInput placeholder="An Amazing Post" onChange={(e) => setTitle(e.target.value)} />
+                    <br/>
+                    <h6>Description</h6>
+                    <FormTextarea onChange={(e) => setDescription(e.target.value)} />
+                    <br/>
                     <p>This is why the next 12 months, I will be pulling back
                     the curtain on my business and providing a raw, real and
                     vulnerable look inside my entrepreneurial journey building
-                    my new agency, Predictable Growth.
-                    </p>
+                    my new agency, Predictable Growth.</p>
                   </CardBody>
-              </Card>
+                </Card>
               </Row>
 
               <Row className='onboardSection'>
                   <Card className='feedarea'>
                       <CardBody>
-                        <h5>Growing Tips</h5>
+                        <h5>Controls</h5>
                         <p>This is why the next 12 months, I will be pulling back
                         the curtain on my business and providing a raw, real and
                         vulnerable look inside my entrepreneurial journey building
                         my new agency, Predictable Growth.
                         </p>
-                        <p>This is why the next 12 months, I will be pulling back
-                        the curtain on my business and providing a raw, real and
-                        vulnerable look inside my entrepreneurial journey building
-                        my new agency, Predictable Growth.
-                        </p>
-                        <p>This is why the next 12 months, I will be pulling back
-                        the curtain on my business and providing a raw, real and
-                        vulnerable look inside my entrepreneurial journey building
-                        my new agency, Predictable Growth.
-                        </p>
+                        <Button outline onClick={() => { SaveContent() }}>Save1</Button>
+                        <Button outline>Save</Button>
+                        <Button outline>Save</Button>
+                        <Button outline>Save</Button>
+                        <Button outline>Save</Button>
+                        <Button outline>Save</Button>
+                        <Button outline>Save</Button>
+                        <Button outline>Save</Button>
                       </CardBody>
                   </Card>
               </Row>
