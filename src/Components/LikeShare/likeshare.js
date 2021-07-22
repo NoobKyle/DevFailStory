5fd72e5a443aca75f97f4076a58461b3d0ae3bd8
kyle <kadangotinaishe5@gmail.com>
2021-07-22 15:57:34 +0200

    Twitter Share

--- src/Components/LikeShare/likeshare.js
+++ src/Components/LikeShare/likeshare.js
@@ -1,21 +1,23 @@
 import React from "react";
-import {
-  Container,
-  Row,
-  Col,
-} from "shards-react";
+import { Container, Row, Col } from "shards-react";
 import { AiOutlineHeart } from 'react-icons/ai';
 import { FiShare, FiSave } from 'react-icons/fi';
 import './likeshare.css';
 
 const LikeShare: React.FC = () => {
+  const postURL = window.location.href;
+
   return (
     <div>
       <hr/>
         <Container className='likesharecontainer'>
           <Row>
             <Col><AiOutlineHeart/></Col>
-            <Col><FiShare/></Col>
+            <Col>
+              <a href={`https://twitter.com/intent/tweet?text=Hey!%20check%20out%20this%20awesome%20post%20I%20found%20on%20%23devfailstory%0D%0D${postURL}`} target="_blank">
+                <FiShare className="sharebutton"/>
+              </a>
+            </Col>
             <Col><FiSave/></Col>
           </Row>
         </Container>
