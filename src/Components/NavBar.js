1094a876aacafe7b95742daebf191887388bf30f
kyle <kadangotinaishe5@gmail.com>
2021-11-08 20:49:59 -0500

    Back Again

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,21 +1,16 @@
 
 import React from "react";
-import {
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-} from "shards-react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "shards-react";
+import "./NavBar.css"
 
 export default class NavBar extends React.Component {
   render() {
     return (
-        <Navbar type="light" expand="md" className='navigationbar' style={{position: "sticky", top: "0", zIndex: "1"}}>
-          <NavbarBrand href="/feed" className='ml-auto'><strong>Developer-Fail-Story</strong></NavbarBrand>
+        <Navbar type="dark" expand="md" className='navigationbar' style={{position: "sticky", top: "0", zIndex: "1"}}>
+          <NavbarBrand href="/feed" className='ml-auto'><strong className="makewhite">Developer-Fail-Story</strong></NavbarBrand>
           <Nav navbar className='ml-auto'>
               <NavItem>
-                <NavLink active style={{fontSize: "1rem"}}>
+                <NavLink active style={{fontSize: "1rem"}} className="makewhite">
                   BETA v1.0
                 </NavLink>
               </NavItem>
